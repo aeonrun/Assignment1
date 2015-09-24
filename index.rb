9b56617c128f2409cdb6d5ef303fa446678f2b4b
Joyce Shen <juanjie@gmail.com>
2015-09-23 21:40:58 -0400

    initial commit

---
+++ index.rb
@@ -0,0 +1,47 @@
+
+
+#add phase to string
+
+holidays = ['halloween', 'veterans', 'july fourth']
+
+holiday_in_us = holidays.collect{|x| x+" is a holiday only in america"}
+
+	puts holiday_in_us
+	
+
+
+#find max number
+numbers = [22, 59, 78, -10]
+
+num1 = numbers[0]
+num2 = numbers[1]
+num3 = numbers[2]
+num4 = numbers[3]
+
+if num1 > num2 && num1 > num3 && num1>num4 
+	puts "#{num1} is max"
+
+elsif num2 > num1 && num2 > num3 && num2>num4 
+	puts "#{num2} is max"
+
+elsif num3 > num1 && num3 > num2 && num3>num4 
+	puts "#{num3} is max"
+
+elsif num4 > num1 && num4 > num2 && num4>num3 
+	puts "#{num4} is max"
+
+else 
+	puts"recalculate"
+end
+
+
+
+#two arguments into a hash
+
+car_brand = [:toyota, :tesla]
+model=["Prius", "Model S"]
+
+car_model = Hash[car_brand.zip(model)]
+
+puts car_model
+
